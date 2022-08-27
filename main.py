8311b03a5dc344d9f988bd7f610b3c89af384456
SMITamim <Stamim22@yahoo.com>
2022-08-27 22:15:41 +0600

    fun project

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pyttsx3 as ptt
 import datetime
 import pywhatkit
 import wikipedia
+import pyjokes
+import subprocess
 
 listener = sr.Recognizer()
 alexa = ptt.init()
@@ -21,6 +23,8 @@ def take_command():
             command = listener.recognize_google(voice)
             if 'alexa' in command:
                 command = command.replace('alexa', '')
+            else:
+                talk("can't hear you")
     except:
         pass
     return command
@@ -35,9 +39,21 @@ def run_alexa():
         song = command.replace('play', '')
         talk('playing '+ song)
         pywhatkit.playonyt(song)
-    elif 'tell me about' or 'info':
+    elif 'tell me about' or 'info' in command:
         look_for = command.replace('tell me about', '') , command.replace('info', '')
         info = wikipedia.summary(look_for, 2)
+        print(info)
         talk(info)
+    elif 'jokes' in command:
+        talk('let me tell you something funny' + pyjokes.get_joke())
+    elif 'on date' in command:
+        talk('yes of course. But we have to do it virtually')
+    elif 'open' in command:
+        app = command.replace('open', '')
+        talk('opening' + app)
+        subprocess.call(app)
+    else:
+        talk('showing relevant results')
+        pywhatkit.search(command)
 
 run_alexa()
